79923e4825f972b1be5af360914350d00a3e42be
arun <apmalani@uci.edu>
2024-09-08 22:49:16 -0700

    implemented eq generation in strat/sup pairings

--- src/equilibria.py
+++ src/equilibria.py
@@ -25,12 +25,26 @@ def indifference(matrix, row_indices, column_indices):
     except np.linalg.LinAlgError as e:
         return False
     
-def support_pairs(row, col):
-    row_strat, col_strat = row.shape
+def support_pairs(matrix):
+    row_strat, col_strat = matrix.shape
 
     for i in custom_powerset(row_strat):
         for j in custom_powerset(col_strat):
                 yield i, j
-    
+
+def support_obedience(strat, sup):
+    if strat is False: return False
+    return all(strat[i] != 0 for i in sup) and all(strat[i] == 0 for i in range(len(strat)) if i not in sup)
+
+def generate_equilibrium(row, col):
+    col_t = col.T
+
+    for row_sup, col_sup in support_pairs(row):
+        row_strats = indifference(col_t, col_sup, row_sup)
+        col_strats = indifference(row, row_sup, col_sup)
+
+        if support_obedience(row_strats, row_sup) and support_obedience(col_strats, col_sup):
+                yield row_strats, col_strats, row_sup, col_sup
+
 def solve_nash_equilibrium(row, col):
     pass
--- tests/test_equilibria.py
+++ tests/test_equilibria.py
@@ -13,17 +13,30 @@ class TestSolving(unittest.TestCase):
         cols = [0, 1]
 
         self.assertTrue(all(np.isclose(src.equilibria.indifference(matrix, rows, cols), np.array([0.5, 0.5]))))
-        
+
     def test_support_pairs(self):
-        row_matrix = np.array([
+        matrix = np.array([
             [1, -1],
             [-1, 1]
-        ])
-        col_matrix = np.array([
-            [-1, 1],
-            [1, -1]
         ])
 
         expected = [((0,), (0,)), ((0,), (1,)), ((0,), (0, 1)), ((1,), (0,)), ((1,), (1,)), ((1,), (0, 1)), ((0, 1), (0,)), ((0, 1), (1,)), ((0, 1), (0, 1))]
 
-        self.assertEqual(list(src.equilibria.support_pairs(row_matrix, col_matrix)), expected)
+        self.assertEqual(list(src.equilibria.support_pairs(matrix)), expected)
+
+    def test_support_obedience(self):
+        self.assertTrue(src.equilibria.support_obedience(np.array([1, -1]), np.array([0, 1])))
+        self.assertFalse(src.equilibria.support_obedience(False, np.array([0, 1])))
+        self.assertFalse(src.equilibria.support_obedience(np.array([0, 100]), np.array([0, 1])))
+
+    def test_equilibria_generation(self):
+        row = np.array([
+            [1, -1],
+            [-1, 1]
+        ])
+        col = np.array([
+            [-1, 1],
+            [1, -1]
+        ])
+
+        print(list(src.equilibria.generate_equilibrium(row, col)))
